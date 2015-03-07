30db752a52029d10bc64ae0f5c5fb77668d9630c
Lili <lilikokalova@gmail.com>
2015-03-07 19:24:54 +0200

    adding twitter

--- sharer/app/controllers/application_controller.rb
+++ sharer/app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #these codes can be modified depends on your needs
+	include Twitter::Login::Helpers
+
+	def current_user
+	  session[:twitter_user] ||= twitter_user
+	end
+	helper_method :current_user
+
+	def logged_in?
+	  !!current_user
+	end
+	helper_method :logged_in?
 end
