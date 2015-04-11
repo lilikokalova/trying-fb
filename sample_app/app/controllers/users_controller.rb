431b2735aae64ef2fa04ebe0cc2b8a7bc11b53d5
Atanaska <a_ivancheva97@abv.bg>
2015-04-12 00:53:33 +0300

    Non-working twitter log-in

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -7,7 +7,10 @@ class UsersController < ApplicationController
   def login
     @user = User.koala(request.env['omniauth.auth']['credentials'])
   end
-
-   
   
+  def create
+   @user[:access_token] = request.env['omniauth.auth']['credentials']['token'] 
+   @user[:access_token_secret] = request.env['omniauth.auth']['credentials']['secret']
+   redirect_to show_path, notice: 'Signed in'
+  end
 end 
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -12,5 +12,32 @@ class User < ActiveRecord::Base
 	#facebook.put_wall_post("hey, i'm learning koala")
 	
   end
+  def self.from_omniauth(auth)
+    user = where(provider: auth.provider, uid: auth.uid).first || create_from_omniauth(auth)
+    user.oauth_token = auth.credentials.token
+    user.oauth_secret = auth.credentials.secret
+    user.save!
+    user
 
-end 
+
+  end
+
+
+  def self.create_from_omniauth(auth)
+    create! do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.username = auth.info.nickname
+      user.image_url = auth.info.image
+      user.save 
+    end
+  end
+
+
+  def twitter
+      @twitter ||= Twitter::Client.new(oauth_token: oauth_token, oauth_token_secret: oauth_secret, access_token: ENV['179516515-t1DSMep9DPord6OanZcBI7Ir151LJ58Tke6S1zw8'], access_token_secret: ENV['RyaFyC1IsrzjPxdLBqPiYodDpmQ5UBb8eZBFarXdmNApV'] ) #ENV's are stored locally. 
+  end
+  
+end
+
+
